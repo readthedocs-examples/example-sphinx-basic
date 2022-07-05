ae7337589080578d7a6747920de77bb12e42153a
Benjamin Bach <benjamin@overtag.dk>
2022-07-05 15:52:41 +0200

    Add rtd to intersphinx, black'n conf.py and lumache.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,46 +17,47 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Basic Sphinx Example Project'
-copyright = '2022, Read the Docs core team'
-author = 'Read the Docs core team'
+project = "Basic Sphinx Example Project"
+copyright = "2022, Read the Docs core team"
+author = "Read the Docs core team"
 
 
 # -- General configuration ---------------------------------------------------
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.duration",
+    "sphinx.ext.doctest",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.intersphinx",
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
+    "python": ("https://docs.python.org/3/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
 }
-intersphinx_disabled_domains = ['std']
+intersphinx_disabled_domains = ["std"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # -- Options for EPUB output
-epub_show_urls = 'footnote'
+epub_show_urls = "footnote"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
--- lumache.py
+++ lumache.py
@@ -17,6 +17,7 @@ __version__ = "0.1.0"
 
 class InvalidKindError(Exception):
     """Raised if the kind is invalid."""
+
     pass
 
 
